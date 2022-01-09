ffedf8d892ed2e11348c8f3daadf356684cb0bfe
melle-issa <mhida010@ucr.edu>
2022-01-09 10:45:09 -0800

    finished test cases

--- test.cpp
+++ test.cpp
@@ -12,6 +12,21 @@ TEST(EchoTest, EmptyString) {
     EXPECT_EQ("", echo(1,test_val));
 }
 
+TEST(EchoTest, OneWord) { //first test case checks to make sure space isn't printed after only one word
+    char* test_val[2]; test_val[0] = "./c-echo"; test_val[1] = "Hello";
+    EXPECT_EQ("Hello", echo(2,test_val));
+}
+
+TEST(EchoTest, EmptyMiddleWord) { //second test case checks behavior if a word is missing in middle
+    char* test_val[4]; test_val[0] = "./c-echo"; test_val[1] = "Hello"; test_val[2] = ""; test_val[3] = "World";
+    EXPECT_EQ("Hello World", echo(4,test_val));
+}
+
+TEST(EchoTest, AllSpaces) { //third test case checks behavior if the whole array is spaces
+    char* test_val[4]; test_val[0] = "./c-echo"; test_val[1] = " "; test_val[2] = " "; test_val[3] = " ";
+    EXPECT_EQ("     ", echo(4,test_val));
+}
+
 int main(int argc, char **argv) {
   ::testing::InitGoogleTest(&argc, argv);
   return RUN_ALL_TESTS();
